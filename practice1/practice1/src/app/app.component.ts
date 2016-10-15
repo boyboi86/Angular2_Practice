d03bfd4deb257e80e92fbe88e524b2f8ddd8a1dc
boyboi86 <boyboi86@gmail.com>
2016-10-16 00:11:03 +0800

    main ts file

---
+++ practice1/practice1/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app works really cool!';
+  pass = 'chicken love duck';
+  delete = false;
+  test = "starting value";
+  boundvalue = 2000;
+}
