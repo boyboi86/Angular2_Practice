fb700923a16ceba31ada06af90807138c126ae47
boyboi86 <boyboi86@gmail.com>
2016-10-20 19:23:39 +0800

    update ReactiveForm

--- practice5/src/app/app.module.ts
+++ practice5/src/app/app.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule  } from '@angular/platform-browser';
 
 import { AppComponent }   from './app.component';
 import { DataDrivenComponent } from "./data-driven/data-driven.component";
 import { TemplateDrivenComponent } from "./template-driven/template-driven.component";
 
-
+//ReactiveFormModule is for data-driven forms
 
 @NgModule({
     declarations: [AppComponent, DataDrivenComponent, TemplateDrivenComponent],
-    imports: [BrowserModule, FormsModule],
+    imports: [BrowserModule, FormsModule, ReactiveFormsModule],
     bootstrap: [AppComponent]
 })
 export class AppModule {}
