8e2eccc7d1514f1f9e54e6f4c21e39947d4b3588
boyboi86 <boyboi86@gmail.com>
2016-10-21 17:30:06 +0800

    update with formbuilder

--- practice5/src/app/data-driven/data-driven.component.ts
+++ practice5/src/app/data-driven/data-driven.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators, FormBuilder, FormArray } from '@angular/forms';
 
 @Component({
     selector: 'data-driven',
@@ -9,17 +9,24 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 export class DataDrivenComponent {
   AForm: FormGroup
   genders= ['male', 'female'];
-  constructor(){
+  constructor(private formBuilder: FormBuilder){
     this.AForm = new FormGroup({
       'userdata': new FormGroup({
         'username': new FormControl('username', Validators.required),
         'email': new FormControl('', [Validators.required, Validators.pattern("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")])
       }),
       'password': new FormControl('', Validators.required),
-      'gender': new FormControl('')
+      'gender': new FormControl(''),
+      'hobbies': new FormArray([
+        new FormControl('Cooking', Validators.required)
+      ])
     })
   }
   onSubmit(){
     return console.log(this.AForm);
+  }
+
+  onAddHobby(){
+    (<FormArray>this.AForm.get('hobbies')).push(new FormControl('', Validators.required));
   }
 }
