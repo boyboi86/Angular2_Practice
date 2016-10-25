24719ca3456ba0b28f5c1c30a4ed237d8ce71bb8
boyboi86 <boyboi86@gmail.com>
2016-10-25 17:15:05 +0800

    added http service calls to firebase

--- practice7/src/app/app.component.ts
+++ practice7/src/app/app.component.ts
@@ -1,12 +1,22 @@
-import { Component, onInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Response } from '@angular/http';
+
 import { HttpService } from './http.service';
+// import { subscribe } from 'rxjs/Rx';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements onInit {
+export class AppComponent implements OnInit {
+  instructions = "kindly refer to developer console if you are using chrome, you will see a call has been made called 'Response'"
   constructor(private httpService: HttpService) {}
 
+  ngOnInit(){
+    this.httpService.getData()
+                    .subscribe((data: Response) => {
+                      console.log(data)
+                    })
+  }
 }
