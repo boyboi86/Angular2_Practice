b3102a92e47afefdaf2c124808ae95de1165196f
boyboi86 <boyboi86@gmail.com>
2016-10-25 21:27:04 +0800

    added new call for route GET

--- practice7/src/app/http.service.ts
+++ practice7/src/app/http.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Http, Response } from '@angular/http';
+import { Http, Response, Headers } from '@angular/http';
 import 'rxjs/Rx';
 
 @Injectable()
@@ -7,9 +7,22 @@ export class HttpService {
 
   constructor(private http: Http) { }
 /*The ending must include json so that firebase can communicate*/
-getData(){
-  return this.http.get('https://angular2-test-f23ee.firebaseio.com/title.json')
-                  .map((response: Response) => response.json());
-}
+  getData(){
+    return this.http.get('https://angular2-test-f23ee.firebaseio.com/title.json')
+                    .map((response: Response) => response.json());
+  }
 
+  sendData(user: any){
+    const body = JSON.stringify(user);
+    const headers = new Headers();
+    headers.append('Content-type','application/json');
+    return this.http.post('https://angular2-test-f23ee.firebaseio.com/data.json', body, {
+      headers: headers
+    })
+    .map((response: Response) => response.json());
+  }
+  retrieveData(){
+    return this.http.get('https://angular2-test-f23ee.firebaseio.com/data.json')
+                    .map((response: Response) => response.json());
+  }
 }
